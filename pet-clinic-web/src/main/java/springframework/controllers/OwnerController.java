2ecee2f15426ddc94ea3eb59337c8708a9540e44
piotr-coder <blichu@wp.pl>
2020-06-02 16:33:48 +0200

    Adding Owner Page and controller. Closes #13

---
+++ pet-clinic-web/src/main/java/springframework/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listOfOwners(){
+        return "owners/index";
+    }
+}
