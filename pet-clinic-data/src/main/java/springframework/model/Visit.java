bdb034b6ea727a6d845981d121da3692b3e9baa7
piotr-coder <blichu@wp.pl>
2020-06-18 13:00:49 +0200

    Added visits, updated owner with pet relationship. Closes #26

---
+++ pet-clinic-data/src/main/java/springframework/model/Visit.java
@@ -0,0 +1,36 @@
+package springframework.model;
+
+import java.time.LocalDate;
+
+/**
+ * Created by Piotr on 2020-06-18
+ */
+public class Visit extends BaseEntity {
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
