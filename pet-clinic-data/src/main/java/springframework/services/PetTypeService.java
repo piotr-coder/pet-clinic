47c74e3a14be39e7e3637edc01bed3f073862db7
piotr-coder <blichu@wp.pl>
2020-06-18 14:00:00 +0200

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/springframework/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package springframework.services;
+
+import springframework.model.PetType;
+
+/**
+ * Created by Piotr on 2020-06-18
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
