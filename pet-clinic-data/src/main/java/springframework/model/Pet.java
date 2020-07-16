e9bf17e22525b244c449a67c35dccceb4adf73df
piotr-coder <blichu@wp.pl>
2020-07-16 16:27:22 +0200

    Added Visit Entity. Closes #38

--- pet-clinic-data/src/main/java/springframework/model/Pet.java
+++ pet-clinic-data/src/main/java/springframework/model/Pet.java
@@ -2,6 +2,8 @@ package springframework.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -16,6 +18,16 @@ public class Pet extends BaseEntity{
     private Owner owner;
     @Column(name = "birth_date")
     private LocalDate birthDate;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 
     public String getName() {
         return name;
--- pet-clinic-data/src/main/java/springframework/model/Visit.java
+++ pet-clinic-data/src/main/java/springframework/model/Visit.java
@@ -1,13 +1,23 @@
 package springframework.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by Piotr on 2020-06-18
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
+
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
