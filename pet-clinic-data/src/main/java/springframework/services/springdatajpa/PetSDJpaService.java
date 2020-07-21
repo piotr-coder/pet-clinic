e6ef0d8dd0c1e519b380aa5c2ea4749a3f1e10e7
piotr-coder <blichu@wp.pl>
2020-07-21 13:52:01 +0200

    Added spring data jpa service for Pet. Closes #43

---
+++ pet-clinic-data/src/main/java/springframework/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,50 @@
+package springframework.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import springframework.model.Pet;
+import springframework.repositories.PetRepository;
+import springframework.services.PetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Piotr on 2020-07-21
+ */
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+    private final PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
