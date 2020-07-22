ed9a655c9c7f0cc61e14ec0795b7cdcf101f8125
piotr-coder <blichu@wp.pl>
2020-07-22 16:00:31 +0200

    Implemented Map impl for Visit Service. Closes #46

---
+++ pet-clinic-data/src/main/java/springframework/services/VisitService.java
@@ -0,0 +1,9 @@
+package springframework.services;
+
+import springframework.model.Visit;
+
+/**
+ * Created by Piotr on 2020-07-22
+ */
+public interface VisitService extends CrudService<Visit, Long>{
+}
